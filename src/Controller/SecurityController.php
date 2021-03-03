00adce744ddedb5a6a6fa2fcd657813e6013d9a7
Manuel-Fami <manuelfamiliar@laposte.net>
2021-03-03 14:43:00 +0100

    Template forgotPwd via lien

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -35,5 +35,14 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
+
+    /**
+     * @Route("/forgotPwd", name="app_forgotPwd")
+     *
+     */
+    public function forgotPwd()
+    {
+        return $this->render('security/forgotPwd.html.twig');
+    }
     
 }
